40b03ccf105cabf6c7a796d5578f14064675fe37
Elsen Ismayilov <elsenismayilov0057@gmail.com>
2022-03-14 14:51:48 +0400

    taske  2 yazildi

---
+++ Js-Task/type.js
@@ -0,0 +1,8 @@
+let a = false;
+if (typeof (a) == "number") {
+    console.log("daxil etdiyiniz deyer ededdir")
+} else if (typeof (a) == "string") {
+    console.log("daxil etdiyiniz deyer string dir")
+} else if (typeof (a) == "boolean") {
+    console.log("daxil etdiyiniz deyer Boolean dir")
+}
