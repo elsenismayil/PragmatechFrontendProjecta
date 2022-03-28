d48faefce2c7cbc02794e97951be596232531593
Elsen Ismayilov <elsenismayilov0057@gmail.com>
2022-03-28 22:38:29 +0400

    taske 1 yazildi

---
+++ JsDataStructures/tv01.js
@@ -0,0 +1,7 @@
+let names = ['Eli', 'Ehmed', 'Sara', 'Sevda', 'Qurban'];
+let surnames = ['Piriyev', 'Memmedov', 'Qurbanova', 'Ezizova', 'Sadiqzade'];
+let ages = [23, 34, 40, 58, 60];
+let students = [names, surnames, ages]
+for(let i = 0;  i<5;i++){
+    console.log(`Ad:${names[i]} , soyad:${surnames[i]} , yaş:${ages[i]}`)
+}
