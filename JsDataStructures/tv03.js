75d22d5cb2e04c1c9c6b7ddf5306360c807479e8
Elsen Ismayilov <elsenismayilov0057@gmail.com>
2022-03-30 12:30:33 +0400

    taske 3 yazildi

---
+++ JsDataStructures/tv03.js
@@ -0,0 +1,30 @@
+// inputs
+
+let students = [{
+    ad: 'Eli',
+    soyad: 'Piriyev',
+    yas: 23
+}, {
+    ad: 'Ehmed',
+    soyad: 'Memmedov',
+    yas: 34
+}, {
+    ad: 'Sara',
+    soyad: 'Qurbanova',
+    yas: 40
+}, {
+    ad: 'Sevda',
+    soyad: 'Ezizova',
+    yas: 58
+}, {
+    ad: 'Qurban',
+    soyad: 'Sadiqzade',
+    yas: 60
+}]
+
+//expected output
+// Ad: Eli,Soyad:Piriyev,Yas:23
+// Ad: Ehmed,Soyad:Memmedov,Yas:34
+for(let i = 0; i<5;i++){
+    console.log(`ad:${students[i].ad},soyad:${students[i].soyad},yas:${students[i].yas}`)
+}
