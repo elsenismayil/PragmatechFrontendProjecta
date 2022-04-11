48d165ac50bd88a202771508d74604362db21877
Elsen Ismayilov <elsenismayilov0057@gmail.com>
2022-04-11 23:45:18 +0400

    extra

--- test/app.js
+++ test/app.js
@@ -1,9 +1,9 @@
-// let a = ['Abbas','menn','sen']
-// for(i in a){
-//   for(j in a[i]){
-//       if(a[i][j]==a[i][j-1]){
-//           console.log(a[i])
-//       }
-//   }
-// }
+let a = ['Abbas','veli','eli']
+for(i in a){
+  for(j in a[i]){
+      if(a[i][j]==a[i][j-1]){
+          console.log(a[i])
+      }
+  }
+}
 
