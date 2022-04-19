36d35bbcfb7a486774cf4cea9de1d2bf8221739b
Elsen Ismayilov <elsenismayilov0057@gmail.com>
2022-04-19 21:20:11 +0400

    taske bitdi

---
+++ taske/Taske05/app.js
@@ -0,0 +1,53 @@
+// function createPoolTable() {
+//     // 1-i ağ 1-i qara 7-si mavi 7-si qırmızı olmaqla ümümilikdə 16 bilyard topu yaradın
+//     // ağ dairə xaricində geri qalan topların daxilində nömrələri yazılacaq
+//     // bilyard masasI görüntüsü üçün [link](https://upload.wikimedia.org/wikipedia/commons/5/5e/American-style_pool_table_diagram_%28empty%29.png)-dəki arxafon şəklindən istifadə edin.
+//     // yaradılan toplar random olaraq görüntüsü verilən bilyard masası üzərində random yerlərdə yerləşməlidir.
+//   }
+let ranL=Math.floor(Math.random()*1400)
+let ranT= Math.floor(Math.random()*750)
+let ag = document.createElement('div')
+ag.className = 'ag'
+ag.style.width = '50px'
+ag.style.height ='50px'
+ag.style.backgroundColor='white'
+ag.style.borderRadius = '50%'
+ag.style.display='flex'
+ag.style.position ='absolute'
+ag.style.marginLeft=`${ranL}px`
+ag.style.marginTop =`${ranT}px`
+let picture = document.querySelector('.picture')
+picture.appendChild(ag)
+let black = document.createElement('div')
+black.className = 'black'
+  for(let i = 0; i<7;i++){
+    let blue = document.createElement('div')
+    blue.className = 'blue'
+    blue.style.width = '50px'
+    blue.style.height ='50px'
+    blue.style.borderRadius = '50%'
+    blue.style.left=`${Math.floor(Math.random()*1400)}px`
+    blue.style.top=`${Math.floor(Math.random()*750)}px`
+    blue.style.display='flex'
+    blue.style.backgroundColor = 'blue'
+    blue.style.margin = '5px'
+    picture.appendChild(blue)
+  }
+
+
+
+for(let j = 0; j<7;j++){
+    let red = document.createElement('div')
+    red.className = 'red'
+    red.style.width = '50px'
+    red.style.height ='50px'
+    red.style.marginLeft= '400px'
+    red.style.borderRadius = '50%'
+    red.style.display='flex'
+    red.style.left= `${Math.floor(Math.random()*1400)}px`
+    red.style.top=`${Math.floor(Math.random()*750)}px`
+    red.style.position ='absolute'
+    red.style.backgroundColor = 'red'
+    red.style.margin = '5px'
+    picture.appendChild(red)
+  }
