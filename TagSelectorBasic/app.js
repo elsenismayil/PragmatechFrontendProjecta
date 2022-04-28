5ed7b657488f6582f7020aafd545141db4f9dd93
Elsen Ismayilov <elsenismayilov0057@gmail.com>
2022-04-28 21:11:00 +0400

    taske yazildi

--- TagSelectorBasic/app.js
+++ TagSelectorBasic/app.js
@@ -1,15 +1,43 @@
-let circle = document.querySelectorAll('.circle')
-let buttons = document.querySelectorAll('.btn')
-    
- function showCircle(i){
-    if(circle[i].style.display ='none'){
-        circle[i].style.display ='block'
-        buttons[i].style.backgroundColor='blue'
-        buttons[i].style.color='white'
-        
-    }else{
-        circle[i].style.display ='none'
-        buttons[i].style.backgroundColor='rgb(214, 228, 232)'
-        buttons[i].style.color ='black'
+
+let input = document.querySelector(".int")
+input.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+
+        event.preventDefault();
+        creatButton()
     }
- }
+});
+let butonsArea = document.querySelector(".buttons-area")
+
+var i = 0;
+function creatButton() {
+    let a = 0
+    a = ++i;
+    butonsArea.innerHTML += `
+    <button onclick="show(${a - 1})" 
+     class="btn middle wid-111  main-func">
+     ${document.querySelector(".int").value}
+
+     <div class="circle"><i class="fa-brands fa-vimeo-v"></i></div>
+     </button>`
+}
+
+function show(i) {
+    let circle = document.querySelectorAll(".circle")
+    let butx = document.querySelectorAll(".main-func")
+    if (circle[i].style.display === 'none') {
+        circle[i].style.display = 'block'
+        butx[i].style.backgroundColor = 'lightblue'
+        butx[i].style.border = 'lightblue'
+
+    } else {
+        circle[i].style.display = 'none'
+        butx[i].style.backgroundColor = 'rgb(208, 198, 198)'
+        butx[i].style.border = 'rgb(208, 198, 198)'
+
+
+
+
+    }
+}
+
